2344edfb019a2ec8b02155a0bd24b1050570b86b
Babasaheb <babs20795@gmail.com>
2020-07-24 11:45:32 +0530

    Real Estate

---
+++ Python/huffman.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 12:47:50 2020
+
+@author: Babs
+"""
+
---
+++ Real_Estate_Price_Prediction/Steps and readme.txt
@@ -0,0 +1,3 @@
+1:  Download dataset
+
+2:  Open Jupyter notebook using anaconda prompt at specific drive
